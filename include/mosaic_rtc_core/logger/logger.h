8ac8d9c00dcb7b15f1701cb31bc637b4f1f62646
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-01-01 13:42:54 +0000

    - Add logger header file: define MOSAIC_RTC_CORE_LOGGER_H and include necessary log headers

---
+++ include/mosaic_rtc_core/logger/logger.h
@@ -0,0 +1,11 @@
+//
+// Created by yhkim on 1/1/26.
+//
+
+#ifndef MOSAIC_RTC_CORE_LOGGER_H
+#define MOSAIC_RTC_CORE_LOGGER_H
+
+#include "log.h"
+#include "webrtc_log.h"
+
+#endif  // MOSAIC_RTC_CORE_LOGGER_H
